1c4456bdb900326f1aa3ba1913841bee56957970
sruti <sruti@openaq.org>
2020-04-02 15:43:02 -0700

    Use SAM to create lambda to create parquet table

---
+++ functions/parquet/create-parquet-table/index.js
@@ -0,0 +1,18 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+
+console.info('Starting create parquet table function');
+
+exports.handler = function (e, ctx, cb) {
+
+    //Add trigger
+
+    //Get S3 bucket, realtime-gzip-test
+
+    //Run CTAS query
+
+
+    //Set up weekly
+
+
+}

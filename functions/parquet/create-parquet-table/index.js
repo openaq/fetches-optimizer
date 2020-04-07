4fcc6a0c9986a5b0960bacdea7eaea7b2343c1ec
sruti <sruti@openaq.org>
2020-04-07 12:35:59 -0700

    Add Athena query to create table

--- functions/parquet/create-parquet-table/index.js
+++ functions/parquet/create-parquet-table/index.js
@@ -1,18 +1,37 @@
-// const axios = require('axios')
-// const url = 'http://checkip.amazonaws.com/';
+'use strict';
+
+const AWS = require('aws-sdk');
+const athena = new AWS.Athena();
 
 console.info('Starting create parquet table function');
 
 exports.handler = function (e, ctx, cb) {
+    let fetchesDatabase = process.env.DATABASE
+    let fetchesTable = process.env.FETCHES_TABLE
+    let parquetTable = process.env.PARQUET_TABLE
+    let s3BucketLocation = process.env.S3_BUCKET_LOCATION
 
-    //Add trigger
+    //TODO: Add trigger for weekly run
 
-    //Get S3 bucket, realtime-gzip-test
+    //TODO: Delete table and folder before re-creating it
 
-    //Run CTAS query
-
-
-    //Set up weekly
-
-
+    console.info("Starting to run ctas query")
+    const params = {
+        QueryString:
+            `CREATE TABLE ${parquetTable} 
+            WITH (format = 'PARQUET', partitioned_by = ARRAY['parameter']) 
+            AS SELECT date.utc as date_utc, date.local as date_local, location, country, value, unit, city, attribution, averagingperiod, coordinates, sourcename, sourcetype, mobile, parameter 
+            FROM ${fetchesTable};`,
+        QueryExecutionContext: {
+            Database: `${fetchesDatabase}`
+        },
+        ResultConfiguration: {
+            OutputLocation: `${s3BucketLocation}`
+        }
+    }
+    athena.startQueryExecution(params, function (err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        //TODO: Proper error handling
+        else console.log("successful", data); // successful response to start the query
+    });
 }

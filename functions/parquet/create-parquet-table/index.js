169b765271ac48565ee1f394e34971539ad495c1
sruti <sruti@openaq.org>
2020-04-13 15:47:04 -0700

    Use athena-express package for queries

--- functions/parquet/create-parquet-table/index.js
+++ functions/parquet/create-parquet-table/index.js
@@ -1,37 +1,45 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-const athena = new AWS.Athena();
+const aws = require('aws-sdk');
+const AthenaExpress = require('athena-express')
+
+const fetchesDatabase = process.env.DATABASE;
+const s3BucketLocation = process.env.S3_BUCKET_LOCATION
+
+const athenaExpressConfig = {
+    aws,
+    db: fetchesDatabase,
+    s3: s3BucketLocation,
+    retry: 30000, // wait 30 seconds before re-checking query status
+    getStats: true,
+    skipResults: true
+};
+
+const athenaExpress = new AthenaExpress(athenaExpressConfig);
 
 console.info('Starting create parquet table function');
 
-exports.handler = function (e, ctx, cb) {
-    let fetchesDatabase = process.env.DATABASE
-    let fetchesTable = process.env.FETCHES_TABLE
-    let parquetTable = process.env.PARQUET_TABLE
-    let s3BucketLocation = process.env.S3_BUCKET_LOCATION
+exports.handler = async function (e, ctx, cb) {
+
+    const fetchesTable = process.env.FETCHES_TABLE
+    const parquetTable = process.env.PARQUET_TABLE
 
     //TODO: Add trigger for weekly run
 
     //TODO: Delete table and folder before re-creating it
 
-    console.info("Starting to run ctas query")
-    const params = {
-        QueryString:
-            `CREATE TABLE ${parquetTable} 
-            WITH (format = 'PARQUET', partitioned_by = ARRAY['parameter']) 
-            AS SELECT date.utc as date_utc, date.local as date_local, location, country, value, unit, city, attribution, averagingperiod, coordinates, sourcename, sourcetype, mobile, parameter 
-            FROM ${fetchesTable};`,
-        QueryExecutionContext: {
-            Database: `${fetchesDatabase}`
-        },
-        ResultConfiguration: {
-            OutputLocation: `${s3BucketLocation}`
-        }
+    const ctasQuery =
+        `CREATE TABLE ${parquetTable} 
+        WITH (format = 'PARQUET', partitioned_by = ARRAY['parameter']) 
+        AS SELECT date.utc as date_utc, date.local as date_local, location, country, value, unit, city, attribution, averagingperiod, coordinates, sourcename, sourcetype, mobile, parameter 
+        FROM ${fetchesTable};`
+
+    try {
+        console.info("Starting to run ctas query")
+        let results = await athenaExpress.query(ctasQuery);
+        return cb(null, results)
     }
-    athena.startQueryExecution(params, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        //TODO: Proper error handling
-        else console.log("successful", data); // successful response to start the query
-    });
+    catch (error) {
+        return cb(error)
+    }
 }
